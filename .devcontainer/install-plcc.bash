367a7dbb29e7da001888832acc148518565b52a3
Stoney Jackson <dr.stoney@gmail.com>
2025-02-02 16:49:49 -0500

    Update install-plcc.bash

--- .devcontainer/install-plcc.bash
+++ .devcontainer/install-plcc.bash
@@ -1,7 +1,4 @@
-if ! type plcc &> /dev/null ; then
-    PLCC_GIT_BRANCH=main \
-            /bin/bash -c \
-            "$(\curl -fsSL https://github.com/ourPLCC/plcc/raw/main/installers/plcc/install.bash)" \
-            >> ~/.bashrc
-    exec bash
-fi
+PLCC_GIT_BRANCH=main \
+        /bin/bash -c \
+        "$(\curl -fsSL https://github.com/ourPLCC/plcc/raw/main/installers/plcc/install.bash)" \
+        >> ~/.bashrc
